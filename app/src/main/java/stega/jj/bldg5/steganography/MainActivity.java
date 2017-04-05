e30a21233359bac46db1aaed72726d7baf277b51
jj30 <jj@jjxbtcrig>
2017-04-04 21:58:10 -0400

    Media scan added

--- app/src/main/java/stega/jj/bldg5/steganography/MainActivity.java
+++ app/src/main/java/stega/jj/bldg5/steganography/MainActivity.java
@@ -5,25 +5,26 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.text.TextUtilsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int RESULT_LOAD_IMAGE = 1;
+    private String strPicturePath;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] perms = {"android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE"};
+        String[] perms = { "android.permission.READ_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE" };
         int permsRequestCode = 200;
         requestPermissions(perms, permsRequestCode);
 
@@ -63,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         // this code decodes
-        /*if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+        /* if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
             Uri selectedImage = data.getData();
             String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
@@ -85,17 +86,17 @@ public class MainActivity extends AppCompatActivity {
         // this code encodes "Top secret message: DOH"
         if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
             Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
             Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
             cursor.moveToFirst();
 
             int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
+            strPicturePath = cursor.getString(columnIndex);
             cursor.close();
 
             ImageView imageView = (ImageView) findViewById(R.id.imgView);
-            Bitmap bitmapOutput = new Encode().encoded(BitmapFactory.decodeFile(picturePath), "This is a secret message: DOH. That is all.");
+            Bitmap bitmapOutput = new Encode().encoded(BitmapFactory.decodeFile(strPicturePath), "This pretty chess player is Maria Hernandez, she's Argentine.");
             SaveToDisk(bitmapOutput);
             imageView.setImageBitmap(bitmapOutput);
         }
@@ -120,7 +121,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void SaveToDisk(Bitmap toDisk) {
         try {
-            String path = Environment.getExternalStorageDirectory().toString();
+            // String path = Environment.getExternalStorageDirectory().toString();
+            String path = absPath(strPicturePath);
+            String originalName = strPicturePath.replace(path, "").replace("/", "");
             OutputStream fOut = null;
 
             File file = new File(path, "withMessage.png"); // the File to save , append increasing numeric counter to prevent files from getting overwritten.
@@ -130,33 +133,40 @@ public class MainActivity extends AppCompatActivity {
             fOut.flush(); // Not really required
             fOut.close(); // do not forget to close the stream
 
-            MediaStore.Images.Media.insertImage(getContentResolver(),file.getAbsolutePath(),file.getName(),file.getName());
+            MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), file.getName(), originalName);
+            // MediaStore.Images.Media.insertImage(getContentResolver(), toDisk, "WithMessage", "With Message");
+
+            // http://www.grokkingandroid.com/adding-files-to-androids-media-library-using-the-mediascanner/
+            MediaScannerConnection.scanFile(
+                    getApplicationContext(),
+                    new String[]{file.getAbsolutePath()},
+                    null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        @Override
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.v("Steganography",
+                                    "file " + path + " was scanned seccessfully: " + uri);
+                        }
+                    });
+
+
+
         } catch (java.io.IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private String absPath(String filePath) {
+        // returns the absolute path to the file
+        String[] pathFolders = filePath.split("/");
+        int nAbsPathLength = pathFolders.length - 1;
+        String[] outputPath = new String[nAbsPathLength];
 
-
-/*
-
-        FileOutputStream out = null;
-        try {
-            out = new FileOutputStream("WITHMESSAGE");
-            toDisk.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
-            // PNG is a lossless format, the compression factor (100) is ignored
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        for (int i = 0; i < nAbsPathLength; i++) {
+            outputPath[i] = pathFolders[i];
         }
-*/
 
+        return TextUtils.join("/", outputPath);
     }
 
 }
